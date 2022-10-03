4e6ee9888064b6767c74351b29f9f367bef927f4
timnoot <82733075+timnoot@users.noreply.github.com>
2022-10-03 16:55:51 +0200

    hashrouter backwards compatibility.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router'
+
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    if (location.hash.startsWith('#/')) { // checking if it's HashRoute
+      router.push(location.hash.substr(1)); // removing the #/ from the hash
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
