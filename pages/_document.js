3e36c9ae7d3c3af0be5f6326504be3069106e63c
timnoot <82733075+timnoot@users.noreply.github.com>
2024-04-04 23:01:25 +0200

    added adsense

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className='scroll-smooth' style={{ scrollBehavior: 'smooth' }}>
-      <Head />
+      <Head >
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5086988366991731"
+          crossorigin="anonymous"></script>
+      </Head >
       <body>
         <Main />
         <NextScript />
