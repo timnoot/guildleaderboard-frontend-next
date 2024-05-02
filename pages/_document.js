1bb170d71fe282c81cf90232024bf31e524c34be
timnoot <82733075+timnoot@users.noreply.github.com>
2024-05-02 21:30:42 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html className='scroll-smooth' style={{ scrollBehavior: 'smooth' }}>
+    <Html className='scroll-smooth' style={{ scrollBehavior: 'smooth' }} lang='en'>
       <Head >
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5086988366991731"
           crossorigin="anonymous"></script>
